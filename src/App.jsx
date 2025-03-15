94437e45507a95d6f2e37b394034a1c02a347b55
Gloria R <norimaqidoe@hotmail.com>
2025-03-14 19:18:27 -0600

    feat: add errors logic

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,60 @@ import useForm from './hooks/useForm'
 
 function App() {
   const [age, setAge] = useState({ years: "--", months: "--", days: "--"});
+  const [errors, setErrors] = useState({ day: "", month: "", year: "" });
 
   const sendData = async (data) => {
+    let newErrors = { day: "", month: "", year: "" };
     const { day, month, year } = data;
-
-    if (!day || !month || !year) {
-      alert("Please, fill all fields!");
-      return;
-    }
+    const today = new Date();
+  
+    if (!day) newErrors.day = "This field is required";
+    if (!month) newErrors.month = "This field is required";
+    if (!year) newErrors.year = "This field is required";
+  
+    if (day && (day < 1 || day > 31)) newErrors.day = "Must be a valid day";
+    if (month && (month < 1 || month > 12)) newErrors.month = "Must be a valid month";
   
     const birthDate = new Date(year, month - 1, day);
-    if (birthDate.toISOString().slice(0, 10) !== `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`) {
-      alert("Please, enter a valid date!");
-      return;
+    if (
+      birthDate.getFullYear() != year ||
+      birthDate.getMonth() + 1 != month ||
+      birthDate.getDate() != day
+    ) {
+      newErrors.day = "Must be a valid date";
     }
-  
-    const today = new Date();
-    let years = today.getFullYear() - birthDate.getFullYear();
-    let months = today.getMonth() - birthDate.getMonth();
-    let days = today.getDate() - birthDate.getDate();
-  
-    if (days < 0) {
-      months -= 1;
-      days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
-    }
-  
-    if (months < 0) {
-      years -= 1;
-      months += 12;
-    }
-  
-    setAge({ years, months, days });
-  }
 
+    if (year > today.getFullYear()) {
+      newErrors.year = "Must be in the past";
+    }
+
+    if (newErrors.day || newErrors.month || newErrors.year) {
+      setErrors(newErrors);
+      return;
+    } else {
+
+      setErrors({ day: "", month: "", year: "" });
+  
+      const today = new Date();
+      let years = today.getFullYear() - birthDate.getFullYear();
+      let months = today.getMonth() - birthDate.getMonth();
+      let days = today.getDate() - birthDate.getDate();
+  
+      if (days < 0) {
+        months -= 1;
+        days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+      }
+  
+      if (months < 0) {
+        years -= 1;
+        months += 12;
+      }
+  
+      setAge({ years, months, days });
+    }
+  };
+  console.log(errors)
+  
   const { input, handleInputChange, handleSubmit } = useForm(sendData, {
     day: '',
     month: '',
